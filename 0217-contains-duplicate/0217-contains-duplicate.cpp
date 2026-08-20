a908cd578468a889a87fd935489128d0a4379d57
saivaishnavidevi <chidrapusaivaishnavidevis@gmail.com>
2026-08-20 21:09:49 +0530

    Time: 99 ms (10.97%), Space: 113.4 MB (17.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std:: sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
         }
-        return false;
+        if(s.size()==nums.size()){
+            return false;
+        }
+        return true;
     }
 };
